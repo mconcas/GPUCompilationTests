2bd188a0d22c5828a548138b9cb95dcf8fc75021
Matteo Concas <matteo.concas@cern.ch>
2023-08-29 18:25:46 +0200

    Fix printout from lib

--- cuda/lib/cudalib.cu
+++ cuda/lib/cudalib.cu
@@ -5,7 +5,7 @@ __constant__ Data d_data;
 
 __global__ void libKernel()
 {
-    printf("Kernel on main executable -> a: %d, b: %f, c: %f\n", d_data.a, d_data.b, d_data.c);
+    printf("Kernel on library -> a: %d, b: %f, c: %f\n", d_data.a, d_data.b, d_data.c);
 }
 
 extern "C" void runLibKernel()
